b861904a7d978145513da059d34c9dcdcf7e24d1
ksh <hyun031916@gmail.com>
2020-09-04 01:49:53 +0900

    #체크박스를 눌렀을 때 동작이 일어나도록 코딩 ("체크박스를 눌렀습니다" 메시지 출력) 내가 직접 짠 코드...

--- app/src/main/java/com/cookandroid/baseapp/MainActivity.java
+++ app/src/main/java/com/cookandroid/baseapp/MainActivity.java
@@ -3,6 +3,7 @@ package com.cookandroid.baseapp;
 import android.os.Bundle;
 import android.view.View;
 import android.widget.Button;
+import android.widget.CheckBox;
 import android.widget.Toast;
 
 import androidx.appcompat.app.AppCompatActivity;
@@ -10,17 +11,27 @@ import androidx.appcompat.app.AppCompatActivity;
 public class MainActivity extends AppCompatActivity {
     Button button1;
 
+    CheckBox check1;
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
 
-        button1 = (Button) findViewById(R.id.button1);
+//        button1 = (Button) findViewById(R.id.button1);
+//
+//        button1.setOnClickListener(new View.OnClickListener() {
+//            @Override
+//            public void onClick(View view) {
+//                Toast.makeText(getApplicationContext(), "버튼을 눌렀어요", Toast.LENGTH_SHORT).show();
+//            }
+//        });
 
-        button1.setOnClickListener(new View.OnClickListener() {
+        check1 = (CheckBox) findViewById(R.id.check1);
+
+        check1.setOnClickListener(new View.OnClickListener(){
             @Override
-            public void onClick(View view) {
-                Toast.makeText(getApplicationContext(), "버튼을 눌렀어요", Toast.LENGTH_SHORT).show();
+            public void onClick(View view){
+                Toast.makeText(getApplicationContext(), "체크박스를 눌렀어요", Toast.LENGTH_SHORT).show();
             }
         });
     }
